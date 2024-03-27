6ba07bd2694ec00ee3a718a8f33a8d29f602f696
Renuka-M-Anjutagi <162134316+Renuka-M-Anjutagi@users.noreply.github.com>
2024-03-27 19:39:20 +0530

    todaytask

---
+++ index.js
@@ -0,0 +1,63 @@
+function label_create(tagname,attr1,attrval1,content)
+{
+    var ele = document.createElement(tagname)
+    ele.setAttribute(attr1, attrval1)
+    ele.innerHTML = content
+    return ele;
+}
+
+function break_create()
+{
+    var break_ele = document.createElement("br")
+    return break_ele
+}
+ function input_create(tagname,attr1,attrval1,idname)
+ {
+    var input_ele =document.createElement(tagname)
+    input_ele.setAttribute(attr1,attrval1)
+    input_ele.className = "main"
+    input_ele.id = idname
+    return input_ele
+ }
+
+ function button_create(tagname,attr1,attrval1,idname,value)
+ {
+    var button_ele =document.createElement(tagname)
+    button_ele.setAttribute(attr1,attrval1)
+    button_ele.className = "main"
+    button_ele.id = idname
+    button_ele.value = value
+    return button_ele
+ }
+
+ var fname_ele = label_create("lable","for","fname","Firstname");
+ var fname_break = break_create()
+ let input_fname = input_create("input","type","text","fname")
+ var input_fname_break = break_create()
+
+ var mname_ele = label_create("lable","for","mname","Middlename");
+ var mname_break = break_create()
+ let input_mname = input_create("input","type","text","mname")
+ var input_mname_break = break_create()
+
+ var lname_ele = label_create("lable","for","lname","Lastname");
+ var lname_break = break_create()
+ let input_lname = input_create("input","type","text","lname")
+ var input_lname_break = break_create()
+
+ var email_ele = label_create("lable","for","email","Email");
+ var lable_break = break_create()
+ let input_email = input_create("input","type","email","Email")
+ var input_break = break_create()
+
+ var password_ele = label_create("lable","for","password","Password");
+ var pass_break = break_create()
+ let input_pass = input_create("input","type","password","Password")
+ var input_pas_break = break_create()
+
+ 
+ let input_type_submit = button_create("button","submit","submit","Submit","Submit")
+ var input_sub_break = break_create()
+
+
+ document.body.append(fname_ele,fname_break,input_fname,input_fname_break,mname_ele,input_mname_break,input_mname,mname_break,lname_ele,lname_break,input_lname,input_lname_break,email_ele,lable_break,input_email,input_break,password_ele,pass_break,input_pass,input_pas_break,input_type_submit,input_sub_break)
